afecac7f7b0e7dcb75f2bb4acf1cec4da590680d
shuvoamin@yahoo.com <shuvoamin@yahoo.com>
2015-10-14 15:32:56 +0100

    namespace refactored

--- DesignPatterns/ObserverPattern/BinaryObserver.cs
+++ DesignPatterns/ObserverPattern/BinaryObserver.cs
@@ -1,11 +1,6 @@
-﻿using DesignPatterns.ObserverPattern;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
-namespace DesignPatterns.MementoPattern
+namespace DesignPatterns.ObserverPattern
 {
     public class BinaryObserver : Observer
     {
